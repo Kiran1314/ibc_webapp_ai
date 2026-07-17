ea61eb890516feb2f8e13008bd39e21bd7248d62
Kiran1314 <kiran.krishnan1391@gmail.com>
2026-07-17 14:10:40 +0530

    ibc_web_ai update6

--- src/app/work/page.js
+++ src/app/work/page.js
@@ -516,16 +516,16 @@ const handlePhotoClick = (item) => {
                 }}
               >
                 {filteredItems[activeVideoIndex].videoUrl && (
-                  <iframe
-                    width="100%"
-                    height="100%"
-                    src={`https://www.youtube.com/embed/${extractYouTubeId(filteredItems[activeVideoIndex].videoUrl)}?autoplay=1&showinfo=0&rel=0&modestbranding=1&controls=1`}
-                    title={filteredItems[activeVideoIndex].title}
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowFullScreen
-                    style={{ width: '100%', height: '100%', display: 'block' }}
-                  />
+               <iframe
+  width="100%"
+  height="100%"
+  src={`https://www.youtube.com/embed/${videoId}?autoplay=1&origin=${typeof window !== 'undefined' ? window.location.origin : ''}`}
+  title={item.title}
+  frameBorder="0"
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+  allowFullScreen
+  style={{ width: '100%', height: '100%', display: 'block' }}
+/>
                 )}
               </div>
 
